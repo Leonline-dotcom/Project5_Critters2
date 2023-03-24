3d333b9dbda0c841bc7c0e8b8ba68022a5d287cb
Leonline-dotcom <shaji.leon@utexas.edu>
2023-03-24 02:55:24 -0500

    Made the Background and MenuItem

---
+++ assignment5/src/main/java/assignment5/MainTitle.java
@@ -0,0 +1,34 @@
+package assignment5;
+
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
+public class MainTitle extends Pane {
+    private Text text;
+
+    public MainTitle(String name) {
+        String spread = "";
+        for (char c : name.toCharArray()) {
+            spread += c + " ";
+        }
+
+        text = new Text(spread);
+        text.setFont(Font.loadFont(MainMenu.class.getResource("res/Penumbra-HalfSerif-Std_35114.ttf").toExternalForm(), 48));
+        text.setFill(Color.BLACK);
+        text.setEffect(new DropShadow(30, Color.WHITE));
+
+        getChildren().addAll(text);
+    }
+
+    public double getTitleWidth() {
+        return text.getLayoutBounds().getWidth();
+    }
+
+    public double getTitleHeight() {
+        return text.getLayoutBounds().getHeight();
+    }
+}
